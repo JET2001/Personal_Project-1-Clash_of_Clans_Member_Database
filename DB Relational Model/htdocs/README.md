1e0bf848d5afe1080c40253fc8b6a18022e5798d
Jonathan Teo <91585955+JET2001@users.noreply.github.com>
2021-10-01 10:43:44 +0800

    Delete DB Relational Model/htdocs directory

--- DB Relational Model/htdocs/README.md
+++
@@ -1,2 +0,0 @@
-### Member Database Implementation
-This section contains all the files for our Database Application.
--- DB Relational Model/htdocs/pdo.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pdo = new PDO('185.27.134.10;port=3306;dbname=b31_28045229_cocascension', 'b31_28045229_b31_28045229', 'a1s2c3e45678');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
